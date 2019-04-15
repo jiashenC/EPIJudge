900ea106d6ff0f2548e6f3091aa4585dba454ac2
Jiashen Cao <caojiashen24@gmail.com>
2019-04-15 00:25:43 -0400

    levenshtein distance

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def levenshtein_distance(A, B):
-    # TODO - you fill in here.
-    return 0
+    """
+        len(A) = a, len(B) = b
+
+        if A[a] = B[b]: E[a] = E[a - 1], B[b] = B[b - 1]
+        else: 1 + min(deletion, insertion, substitution)
+    """
+    table = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+    for i in range(len(A) + 1):
+        table[i][0] = i
+    for i in range(len(B) + 1):
+        table[0][i] = i
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            if A[i - 1] == B[j - 1]:
+                table[i][j] = table[i - 1][j - 1]
+            else:
+                table[i][j] = 1 + min(table[i - 1][j - 1], min(table[i][j - 1], table[i - 1][j]))
+    return table[-1][-1]
 
 
 if __name__ == '__main__':
