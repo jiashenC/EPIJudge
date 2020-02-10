226402037cfd155fb051951c7e23cb12a895c71f
Jiashen Cao <caojiashen24@gmail.com>
2020-02-10 14:39:19 -0500

    checkpoint for space

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,10 +1,34 @@
 from test_framework import generic_test
+import collections
 
 
 # Uses BFS to find the least steps of transformation.
 def transform_string(D, s, t):
-    # TODO - you fill in here.
-    return 0
+    matrix = collections.defaultdict(lambda: [])
+    words = list(D)
+    for k, w1 in enumerate(words):
+        for w2 in words[k:]:
+            diff = 0
+            for i in range(len(w1)):
+                diff += 1 if w1[i] != w2[i] else 0
+                if diff > 1:
+                    break
+            if diff <= 1:
+                matrix[w1].append(w2)
+                matrix[w2].append(w1)
+
+    visited = set([])
+    queue = collections.deque([(0, s)])
+    while queue:
+        dis, word = queue.popleft()
+        if word in visited:
+            continue
+        if word == t:
+            return dis
+        visited.add(word)
+        for next in matrix[word]:
+            queue.append((dis + 1, next))
+    return -1
 
 
 if __name__ == '__main__':
